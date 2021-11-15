34791779b6aafcfe03aedb4139b49cebb7c24c92
ccgeller <86666932+ccgeller@users.noreply.github.com>
2021-11-15 16:38:56 -0500

    Update src/js/experiments/relatedResources.js
    Co-authored-by: Tony Shen  <57234605+tshen815@users.noreply.github.com>

--- src/js/experiments/relatedResources.js
+++ src/js/experiments/relatedResources.js
@@ -1,7 +1,7 @@
 $(() => {
   // https://app.optimizely.com/v2/projects/16812830475/experiments/20872380274/variations
   window.OptimizelyClient.getVariationName({
-    experimentKey: 'dd_docs_related_resources_mvp_test_rerun',
+    experimentKey: 'dd_docs_related_resources_mvp_test_rerun_test',
     groupExperimentName: 'q4_fy22_docs_disco_experiment_group_test',
     attributes: {
       // https://app.optimizely.com/v2/projects/16812830475/audiences/20680670160
